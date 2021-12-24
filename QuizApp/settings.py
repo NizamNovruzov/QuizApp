6e4162afd8b77eda2acdd6122fd8c8f95d97f0b4
NizamNovruzov <nizam_novruzov@mail.ru>
2021-12-24 15:31:06 +0400

    fsad

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -75,14 +75,10 @@ WSGI_APPLICATION = 'QuizApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
-}
-DATABASES['default'] =  dj_database_url.config()
-
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+}pwd
 
 
 # Password validation
